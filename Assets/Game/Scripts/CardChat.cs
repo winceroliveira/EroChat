63b64ea0bd62762e2de46b34c59f668876d09aec
Wincer Oliveira <85763225+winceroliveira@users.noreply.github.com>
2024-02-28 23:21:27 -0300

    Starting Projects

---
+++ Assets/Game/Scripts/CardChat.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardChat : MonoBehaviour
+{
+    public Image imageCard;
+    public TMP_Text nameCard;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
